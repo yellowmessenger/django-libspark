f18d3f79508926cfec7053001a324ddc9c63d1ad
Rashid Khan <rashood.khan@gmail.com>
2015-08-23 09:39:55 +0530

    Added support for CORS to make Angular send requests to Django Server

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'api_browser',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +125,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+# CORS Headers
+CORS_ORIGIN_ALLOW_ALL = True
